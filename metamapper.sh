3361df66a3409d9a5bded6ea2d00731cafaf6e9c
Yahweasel <git-rpo@yahweasel.com>
2022-08-04 11:57:36 -0400

    -

--- metamapper.sh
+++ metamapper.sh
@@ -2,7 +2,7 @@
 while true
 do
     clear
-    printf 'Current maps: '
+    /usr/bin/printf '\x1b[m\x1b[97mCurrent maps: '
     ls *.map 2> /dev/null | sed 's/\.map//' | fmt -w 1000
 
     printf '\n'
