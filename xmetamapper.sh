5a1b1b995b3431230000e06cba21e0f2822dbf4c
Yahweasel <git-rpo@yahweasel.com>
2022-08-05 00:33:51 -0400

    Rescaling again

--- xmetamapper.sh
+++ xmetamapper.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-exec sakura \
-    -f 'Pet Me 64 Bold 12' \
-    -c 44 -r 33 \
-    -t Mapper \
+exec env \
+    GDK_SCALE=1 \
+    sakura \
+    -f 'Pet Me 64 Bold 16' \
+    -c 44 -r 32 \
     -e screen -S mapper ./metamapper.sh
